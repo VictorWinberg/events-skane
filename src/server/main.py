69cca79258726ae1391bc9d98b155106bd7241a9
Anton Björkman <antonidag@hotmail.com>
2024-07-31 15:15:01 +0200

    add simple api with ollama integration

--- src/server/main.py
+++ src/server/main.py
@@ -1,12 +1,28 @@
 from typing import Union
 from fastapi import FastAPI
+import ollama
+from ollama import Client
 
+MODEL = "qwen2:0.5b"
 app = FastAPI()
+# Why use the client, since we have this crazy networking going on
+client = Client(host="http://ollama:11434")
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.get("/weather/")
+def generate_prompt(country: str):
+    user_query = f"What is the weather in {country}?"
+    output = llm_generate_prompt(MODEL, user_query)
+    return {"Message": output}
+
+
+def llm_generate_prompt(model, query):
+    print("Start generate response from LLM", flush=True)
+    response = client.generate(model=model, prompt=query)
+    print(response, flush=True)
+    return response
