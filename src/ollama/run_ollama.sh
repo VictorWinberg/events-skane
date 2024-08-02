91e65c75531f8f30884fc6446278621c76263aa6
Victor Winberg <victor.m.winberg@gmail.com>
2024-08-02 11:09:43 +0200

    feat(docker): depends on (#4)

--- src/ollama/run_ollama.sh
+++ src/ollama/run_ollama.sh
@@ -2,10 +2,24 @@
 
 echo "Starting Ollama server..."
 ollama serve &
-ollama run qwen2:0.5b
+SERVER_PID=$!
 
-
-echo "Waiting for Ollama server to be active..."
-while [ "$(ollama list | grep 'NAME')" == "" ]; do
+echo "Waiting for Ollama server to start..."
+while ! cat < /dev/null > /dev/tcp/localhost/11434; do
   sleep 1
+done
+
+ollama pull qwen2:0.5b
+
+echo "Waiting for the model to be downloaded..."
+while ! ollama list | grep -q 'qwen2:0.5b'; do
+  sleep 1
+done
+
+echo "Ollama server is ready!"
+touch /tmp/ollama_ready
+
+echo "Keeping the Ollama server running..."
+while kill -0 "$SERVER_PID" 2>/dev/null; do
+    sleep 10
 done
